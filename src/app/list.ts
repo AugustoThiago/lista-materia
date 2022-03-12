fe087ad3e2975b6f8e18b7d67ebeb6f9754d7e76
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-03-11 21:31:13 -0300

    2 files modified, 3 added

---
+++ src/app/list.ts
@@ -0,0 +1,5 @@
+export const ListData = [
+  {id: 1, nome: 'Materia 01', dia: 'Quinta-Feira', hora: '19h00'},
+  {id: 2, nome: 'Materia 03', dia: 'Quinta-Feira', hora: '19h00'},
+  {id: 3, nome: 'Materia 02', dia: 'Quinta-Feira', hora: '19h00'},
+]
