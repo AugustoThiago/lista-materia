90dad288ddd98123fe273c2b659ff6e78e24b5eb
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-03-11 22:17:43 -0300

    4 files modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,29 +1,29 @@
 export const ListData = [
   {
     id: 1,
-    nome: 'Materia 01',
+    nome: 'Desenvolvimento para Servidores II',
     dia: 'Quinta-Feira',
     hora: '19h00',
-    dados: 'Dados 01',
-    ementa: 'Ementa 01',
-    objetivos: 'Objetivos 01'
+    dados: 'IDS002A-CIRO CIRNE TRINDADE',
+    ementa: 'Conceitos e evolução das tecnologias de programação de servidores. Recursos da linguagem escolhida para servidores na Internet. Padrões de projetos. Integração com sistemas (Google Maps API, Twitter, entre outros',
+    objetivos: 'Desenvolver um site completo de e-commerce ou outro tipo de negócio na Internet usando uma linguagem apropriada a servidores, banco de dados e padrões de projeto. Objetivos específicos. Implementar softwares do lado servidor e com uso de uma linguagem de programação e de padrões de projetos mais usuais como MVC, DAO, Composite, Singleton, entre outros'
   },
   {
     id: 2,
-    nome: 'Materia 03',
-    dia: 'Quinta-Feira',
+    nome: 'Tópicos Especiais em Sistemas para Internet II',
+    dia: 'Sexta-Feira',
     hora: '19h00',
-    dados: 'Dados 02',
-    ementa: 'Ementa 02',
-    objetivos: 'Objetivos 02'
+    dados: 'Tópicos Especiais em Sistemas para Internet II',
+    ementa: 'Recursos e aplicações da tecnologia escolhida',
+    objetivos: 'Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade. Objetivos específicos. Estudar e aplicar tecnologias de uso corrente na região na qual o curso de insere ou de interesse para o desenvolvimento profissional dos estudantes ou desenvolver temas que aprofundem algum conhecimento já estudado'
   },
   {
     id: 3,
-    nome: 'Materia 02',
-    dia: 'Quinta-Feira',
+    nome: 'Projeto de Trabalho de Graduação em Sistemas para Internet I',
+    dia: 'Sábado',
     hora: '19h00',
-    dados: 'Dados 03',
-    ementa: 'Ementa 03',
-    objetivos: 'Objetivos 03'
+    dados: 'TTG401A -MAURICIO CONCEIÇÃO MARIO',
+    ementa: 'Conceito de portfólio no âmbito da educação superior. Planejamento e organização de portfólios',
+    objetivos: 'Apoiar o estudante na organização de seu portfólio de projetos desenvolvidos ao longo do curso. Objetivos específicos. Os estudantes devem ser capazes de organizar o portfólio individual dos projetos desenvolvidos ao longo do curso. Devem ser capazes de redigir resumos, artigos e relatórios técnicos dos trabalhos em linguagem apropriada e na forma culta. Deve demonstrar independência de pensamento e autonomia para pesquisa'
   },
 ];
