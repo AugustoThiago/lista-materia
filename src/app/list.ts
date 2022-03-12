b4c053110ff0895e51fcd1af65258d77aaa3cfc9
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-03-11 21:54:40 -0300

    2 files modified, 1 added

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,5 +1,29 @@
 export const ListData = [
-  {id: 1, nome: 'Materia 01', dia: 'Quinta-Feira', hora: '19h00'},
-  {id: 2, nome: 'Materia 03', dia: 'Quinta-Feira', hora: '19h00'},
-  {id: 3, nome: 'Materia 02', dia: 'Quinta-Feira', hora: '19h00'},
-]
+  {
+    id: 1,
+    nome: 'Materia 01',
+    dia: 'Quinta-Feira',
+    hora: '19h00',
+    dados: 'Dados 01',
+    ementa: 'Ementa 01',
+    objetivos: 'Objetivos 01'
+  },
+  {
+    id: 2,
+    nome: 'Materia 03',
+    dia: 'Quinta-Feira',
+    hora: '19h00',
+    dados: 'Dados 02',
+    ementa: 'Ementa 02',
+    objetivos: 'Objetivos 02'
+  },
+  {
+    id: 3,
+    nome: 'Materia 02',
+    dia: 'Quinta-Feira',
+    hora: '19h00',
+    dados: 'Dados 03',
+    ementa: 'Ementa 03',
+    objetivos: 'Objetivos 03'
+  },
+];
